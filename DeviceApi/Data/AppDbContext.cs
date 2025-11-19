eb066b910bbbbb840a754b83546b2a366a99afa6
Guilherme Freire <gfreire1992@gmail.com>
2025-11-19 19:46:54 -0300

    chore: add initial AddDbContext

---
+++ DeviceApi/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+using DeviceApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DeviceApi.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) {}
+
+        public DbSet<Device> Devices { get; set; }
+    }
+}
