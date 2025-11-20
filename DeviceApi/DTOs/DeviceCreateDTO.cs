5ac6459e9106dfa64499d233918a1a1ec55e6899
Guilherme Freire <gfreire1992@gmail.com>
2025-11-20 17:23:45 -0300

    chore: add DeviceCreateDTO class
    - Create this class to use it on the POST /device endpoint to avoid the user to send id or CreationTime

---
+++ DeviceApi/DTOs/DeviceCreateDTO.cs
@@ -0,0 +1,9 @@
+namespace DeviceApi.Dtos
+{
+    public class DeviceCreateDTO
+    {
+        public string? Name { get; set; }
+        public string? Brand { get; set; }
+        public string? State { get; set; }
+    }
+}
