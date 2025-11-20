ef88aa56a6e4dfa3e817f2d22ae9b897611bcc95
Guilherme Freire <gfreire1992@gmail.com>
2025-11-20 16:55:00 -0300

    chore: init DB on Program.cs

--- DeviceApi/Program.cs
+++ DeviceApi/Program.cs
@@ -1,10 +1,23 @@
+using DeviceApi.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseNpgsql(connectionString));
+
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
