dd30db13c68f2b0294b45ab0ffe825d7117e7b8f
Guilherme Freire <gfreire1992@gmail.com>
2025-11-21 18:33:03 -0300

    feat: create TestDeviceHandlers class with CreateDevice_Ok test
    - I'm testing CreateDevice handler, mocking IDeviceService, then I'm asserting httpResult value and location

---
+++ DeviceApi.Tests/Handlers/TestDeviceHandlers.cs
@@ -0,0 +1,61 @@
+using DeviceApi.Data;
+using DeviceApi.Dtos;
+using DeviceApi.Endpoints;
+using DeviceApi.Models;
+using DeviceApi.Services;
+using DeviceApi.Tests.Helpers;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Moq;
+using Xunit;
+
+namespace DeviceApi.Tests.Handlers
+{
+    public class TestDeviceHandlers
+    {
+       [Fact]
+        public async Task CreateDevice_Ok()
+        {
+            // Arrange
+            var mockService = new Mock<IDeviceService>();
+
+            var dto = GenerateTestDeviceCreateDTO();
+            var createdDevice = GenerateDeviceResult();
+
+            mockService
+                .Setup(s => s.CreateAsync(It.IsAny<Device>()))
+                .ReturnsAsync(createdDevice);
+
+            // Act
+            var result = await DeviceEndpoints.CreateDevice(mockService.Object, dto);
+            var httpResult = Assert.IsType<Created<Device>>(result);
+
+            // Assert
+            Assert.Equal(createdDevice, httpResult.Value);
+            Assert.Equal("/devices/1", httpResult.Location);
+        }
+
+        //aux
+        private Device GenerateDeviceResult()
+        {
+            return new Device 
+            { 
+                Id = 1,
+                Name = "device_name",
+                Brand = "device_brand",
+                State = "device_state",
+                CreationTime = DateTime.UtcNow
+            };
+        }
+
+        private DeviceCreateDTO GenerateTestDeviceCreateDTO()
+        {
+            return new DeviceCreateDTO
+            { 
+                Name = "device_name",
+                Brand = "device_brand",
+                State = "device_state",
+            };
+        }
+    }
+
+}
