aa1d61327d39e72815a4d39064b0b7362f05bc27
Guilherme Freire <gfreire1992@gmail.com>
2025-11-23 19:19:04 -0300

    chore: add DeviceUpdateDTO

---
+++ DeviceApi/DTOs/DeviceUpdateDTO.cs
@@ -0,0 +1,9 @@
+namespace DeviceApi.Dtos
+{
+    public class DeviceUpdateDTO
+    {
+        public string? Name { get; set; }
+        public string? Brand { get; set; }
+        public string? State { get; set; }
+    }
+}
