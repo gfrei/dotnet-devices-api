2ae5bc25fbb9d0a287f71bd9e35290e4a7c8ecbd
Guilherme Freire <gfreire1992@gmail.com>
2025-11-23 17:56:55 -0300

    fix: update IDeviceService

--- DeviceApi/Services/IDeviceService.cs
+++ DeviceApi/Services/IDeviceService.cs
@@ -6,7 +6,7 @@ namespace DeviceApi.Services
     public interface IDeviceService
     {
         Task<Device?> GetByIdAsync(int id);
-        Task<Device> CreateAsync(Device device);
+        Task<Device?> CreateAsync(Device device);
         Task<DeleteResult> DeleteAsync(int id);
         Task<IEnumerable<Device>> QueryAsync(string? name, string? brand, string? state);
     }
