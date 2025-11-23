99105eef3bf319702e3260c38c7f8992722fcc85
Guilherme Freire <gfreire1992@gmail.com>
2025-11-23 18:10:29 -0300

    chore: add DeviceStates
    - To unify the state strings and avoid magic strings

---
+++ DeviceApi/Models/DeviceStates.cs
@@ -0,0 +1,9 @@
+namespace DeviceApi.Data
+{
+    public static class DeviceStates
+    {
+        public const string Available = "available";
+        public const string InUse = "in-use";
+        public const string Inactive = "inactive";
+    }
+}
